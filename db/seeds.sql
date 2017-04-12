51e5ada2d9e5226e3ff6a32696b4e132796e3616
StevenS125 <sts12589@gmail.com>
2017-04-12 12:21:19 -0400

    getting files going to right repo

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rodeo', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sunrise', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Snydz', false);
